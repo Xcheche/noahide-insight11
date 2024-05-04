649e1340c9b9b118619c8f903bb23b1b8d390b0a
xcheche <chekwubechukwu23@gmail.com>
2024-05-04 14:08:38 +0100

    done

---
+++ scroll-to-top.js
@@ -0,0 +1,49 @@
+const scrollToTopBtn = document.getElementById('scroll-to-top');
+
+window.onscroll = function() {
+  const scrollTop = (document.body.scrollTop || document.documentElement.scrollTop);
+
+  if (scrollTop > 100) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+};
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+// form submition
+function submitForm(event) {
+    event.preventDefault();
+
+    // Assuming the form submission is successful
+    // You can add your own form submission logic here if needed
+
+    // Show the success message
+    var successMessage = document.getElementById("success-message");
+    successMessage.innerText = "Sent successfully! \nWe will get back to you in 24 hours.";
+    successMessage.style.display = "flex";
+
+    // Set a timeout to hide the success message after 5 seconds
+    setTimeout(function () {
+        successMessage.style.display = "none";
+    }, 5000);
+
+    // Reset the form fields (optional)
+    event.target.reset();
+
+    return false;
+}
+
+
+// refresh page
+function refreshPage() {
+    location.reload();
+}
+hello()
